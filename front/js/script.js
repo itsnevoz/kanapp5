03f45dbf6fae6ad0089ea260786bacc577c62d20
Itsnevoz <nevoz.sony@gmail.com>
2022-12-20 22:29:26 +0100

    1inner test

--- front/js/script.js
+++ front/js/script.js
@@ -13,8 +13,16 @@ fetch("http://localhost:3000/api/products")
   })
   // dans le cas d'une erreur remplace le contenu de titre par un h1 au contenu de erreur 404 et renvoit en console l'erreur.
   .catch((err) => {
-    document.querySelector(".titles").innerHTML = "<h1>erreur 404</h1>";
-    console.log("erreur 404, sur ressource api:" + err);
+    let erreur = document.querySelector(".titles");
+    let parentErreur = document.getElementById("err");
+    // console.log(parentErreur);
+
+    let newErr = document.createElement("h1");
+    newErr.innerHTML = "error 404";
+
+    parentErreur.replaceChild(newErr, erreur);
+
+    // console.log("erreur 404, sur ressource api:" + err);
   });
 //----------------------------------------------------------------------
 // fonction d'affichage des produits de l'api sur la page index
